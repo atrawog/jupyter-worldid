078b6d0a55f6b6ef0882a1754bebe6b0120bdcc8
Andreas Trawoeger <atrawog@gmail.com>
2024-05-25 20:48:06 +0000

    Cleanup config

--- Makefile
+++ Makefile
@@ -1,83 +1,77 @@
-# Makefile for managing Docker Compose services
+# Makefile for managing COMPOSE_PROJECT_NAME=$(PROJECT) docker compose services
 
 COMPOSE_FILE=docker-compose.yml
 COMPOSE_FILE_PROD=docker-compose.prod.yml
 PROFILE_ARG=--profile
+PROJECT=jupyter-devel
+PROJECT_PROD=jupyter-prod
 
 SERVICES ?= jupyterhub erigon monitoring
 BUILD ?= jupyterhub monitoring buildonly
 
-.PHONY: build build-prod pull pull-prod start start-prod stop stop-prod down down-prod destroy destroy-prod log log-prod prune
+.PHONY: build build-prod pull pull-prod up up-prod stop stop-prod down down-prod destroy destroy-prod logs logs-prod prune
+
+define DOCKER_COMPOSE
+	COMPOSE_PROJECT_NAME=$(PROJECT) docker compose -f $(COMPOSE_FILE) $(foreach service,$(SERVICES),$(PROFILE_ARG) $(service)) $1 $2 $3 $4
+endef
+
+define DOCKER_COMPOSE_PROD
+	COMPOSE_PROJECT_NAME=$(PROJECT_PROD) docker compose --env-file=.env --env-file=.env.prod -f $(COMPOSE_FILE) -f $(COMPOSE_FILE_PROD) $(foreach service,$(SERVICES),$(PROFILE_ARG) $(service)) $1 $2 $3 $4
+endef
+
+define DOCKER_COMPOSE_BUILD
+	COMPOSE_PROJECT_NAME=$(PROJECT) docker compose -f $(COMPOSE_FILE) $(foreach service,$(BUILD),$(PROFILE_ARG) $(service)) $1 $2 $3 $4
+endef
+
+define DOCKER_COMPOSE_PROD_BUILD
+	COMPOSE_PROJECT_NAME=$(PROJECT_PROD) docker compose --env-file=.env --env-file=.env.prod -f $(COMPOSE_FILE) -f $(COMPOSE_FILE_PROD) $(foreach service,$(BUILD),$(PROFILE_ARG) $(service)) $1 $2 $3 $4
+endef
 
 build:
-	for service in $(BUILD); do \
-		docker compose -f $(COMPOSE_FILE) $(PROFILE_ARG) $$service build; \
-	done
+	$(call DOCKER_COMPOSE_BUILD,build)
 
 build-prod:
-	for service in $(BUILD); do \
-		docker compose -f $(COMPOSE_FILE_PROD) $(PROFILE_ARG) $$service build; \
-	done
+	$(call DOCKER_COMPOSE_PROD_BUILD,build)
 
 pull:
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE) $(PROFILE_ARG) $$service pull; \
-	done
+	$(call DOCKER_COMPOSE,pull)
 
 pull-prod:
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE_PROD) $(PROFILE_ARG) $$service pull; \
-	done
+	$(call DOCKER_COMPOSE_PROD,pull)
 
-start: build pull
+up: build pull
 	mkdir -p mnt/devel/erigon
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE) $(PROFILE_ARG) $$service up -d --remove-orphans; \
-	done
+	$(call DOCKER_COMPOSE,up,-d --remove-orphans)
 
-start-prod: build-prod pull-prod
+up-prod: build-prod pull-prod
 	mkdir -p mnt/devel/{erigon,letsencrypt}
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE_PROD) $(PROFILE_ARG) $$service up -d; \
-	done
+	$(call DOCKER_COMPOSE_PROD,up,-d)
 
 stop:
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE) $(PROFILE_ARG) $$service stop; \
-	done
+	$(call DOCKER_COMPOSE,stop)
 
 stop-prod:
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE_PROD) $(PROFILE_ARG) $$service stop; \
-	done
+	$(call DOCKER_COMPOSE_PROD,stop)
 
 down:
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE) $(PROFILE_ARG) $$service down; \
-	done
+	$(call DOCKER_COMPOSE,down)
 
 down-prod:
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE_PROD) $(PROFILE_ARG) $$service down; \
-	done
+	$(call DOCKER_COMPOSE_PROD,down)
 
 destroy: stop
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE) $(PROFILE_ARG) $$service down -v; \
-	done
+	$(call DOCKER_COMPOSE,down,-v)
 	$(MAKE) prune
 
 destroy-prod: stop-prod
-	for service in $(SERVICES); do \
-		docker compose -f $(COMPOSE_FILE_PROD) $(PROFILE_ARG) $$service down -v; \
-	done
+	$(call DOCKER_COMPOSE_PROD,down,-v)
 	$(MAKE) prune
 
 logs:
-	docker compose -f $(COMPOSE_FILE) $(foreach service,$(SERVICES),$(PROFILE_ARG) $(service)) logs -f
+	$(call DOCKER_COMPOSE,logs,-f)
 
 logs-prod:
-	docker compose -f $(COMPOSE_FILE_PROD) $(foreach service,$(SERVICES),$(PROFILE_ARG) $(service)) logs -f
-	
+	$(call DOCKER_COMPOSE_PROD,logs,-f)
+
 prune:
 	docker container prune -f
--- docker/jupyterhub/jupyterhub_config.py
+++ docker/jupyterhub/jupyterhub_config.py
@@ -1,6 +1,8 @@
 import os
 import json
 import urllib.parse
+import hashlib
+import base64
 
 from oauthenticator.oauth2 import OAuthenticator
 from tornado import httpclient
@@ -11,6 +13,7 @@ class WorldIDOAuthenticator(OAuthenticator):
     authorize_url = "https://id.worldcoin.org/authorize"
     token_url = "https://id.worldcoin.org/token"
     userdata_url = "https://id.worldcoin.org/userinfo"
+    introspect_url = "https://id.worldcoin.org/introspect"  # Assuming there is an introspection endpoint
     scope = ['openid', 'profile', 'email']
     username_map = {}
 
@@ -40,11 +43,33 @@ class WorldIDOAuthenticator(OAuthenticator):
                 app_log.error("Failed to retrieve access token")
                 return None
 
+            # Validate the access token
+            # validation_result = await self.validate_access_token(http_client, access_token)
+            # if not validation_result:
+            #     app_log.error("Invalid access token")
+            #     return None
+
+            # Fetch user information with the validated access token
             req = httpclient.HTTPRequest(self.userdata_url, method="GET", headers={"Authorization": f"Bearer {access_token}"})
             user_response = await http_client.fetch(req)
             user_data = json.loads(user_response.body)
             app_log.debug(f"Received user data: {user_data}")
-            return {'name': user_data.get('sub')}
+
+            # Hash the 'sub' field using SHA-256 and then base64 encode it
+            sub = user_data.get('sub')
+            hashed_sub = hashlib.sha256(sub.encode('utf-8')).digest()
+            base64_hashed_sub = base64.urlsafe_b64encode(hashed_sub).decode('utf-8')
+
+            # Return a more comprehensive user dictionary
+            user_info = {
+                'name': base64_hashed_sub,
+                'email': user_data.get('email'),
+                'fullname': user_data.get('name'),
+                'profile': user_data.get('profile'),
+                'scope': self.scope
+            }
+            app_log.debug(f"user info: {user_info}")
+            return user_info
 
         except httpclient.HTTPError as e:
             app_log.error(f"HTTPError fetching access token: {str(e)}")
@@ -53,20 +78,46 @@ class WorldIDOAuthenticator(OAuthenticator):
 
         return None
 
+    async def validate_access_token(self, http_client, access_token):
+        """
+        Validates the access token using the introspection endpoint.
+        """
+        try:
+            body = urllib.parse.urlencode({
+                "token": access_token,
+                "client_id": self.client_id,
+                "client_secret": self.client_secret
+            })
+            headers = {"Content-Type": "application/x-www-form-urlencoded"}
+            app_log.debug(f"Validating access token with body: {body}")
+
+            response = await http_client.fetch(self.introspect_url, method="POST", headers=headers, body=body)
+            introspection_result = json.loads(response.body)
+            app_log.debug(f"Introspection result: {introspection_result}")
+
+            # Return the "active" status of the token
+            return introspection_result.get("active", False)
+        except httpclient.HTTPError as e:
+            app_log.error(f"HTTPError during token introspection: {str(e)}")
+        except Exception as e:
+            app_log.error(f"Error during token introspection: {str(e)}")
+
+        return False
+
 c.JupyterHub.authenticator_class = WorldIDOAuthenticator
 c.WorldIDOAuthenticator.client_id = os.environ.get('OAUTH_CLIENT_ID')
 c.WorldIDOAuthenticator.client_secret = os.environ.get('OAUTH_CLIENT_SECRET')
 c.WorldIDOAuthenticator.oauth_callback_url = os.environ.get('OAUTH_CALLBACK_URL', 'http://localhost:8000/hub/oauth_callback')
-c.WorldIDOAuthenticator.admin_users = {'0x1dd9a0e2e874145ab5c4873b9285d08cb9c5fc5328aaee1dc329e2158dc68349'} # https://simulator.worldcoin.org/id/0x18310f83
+c.WorldIDOAuthenticator.admin_users = { os.environ.get('JUPITER_ADMIN') } 
 c.WorldIDOAuthenticator.allow_all = True
 
 c.JupyterHub.debug_proxy = False
-c.JupyterHub.log_level = 'DEBUG'
+c.JupyterHub.log_level = os.environ.get('JUPITER_LOGLEVEL')
 c.JupyterHub.ip = '0.0.0.0'
 c.JupyterHub.port = 8000
 c.JupyterHub.hub_ip = 'jupyterhub'
 
 c.JupyterHub.spawner_class = 'dockerspawner.DockerSpawner'
-c.DockerSpawner.network_name = 'jupyter-devel'
-c.DockerSpawner.image = 'blockscience:latest'
+c.DockerSpawner.network_name = os.environ.get('JUPITER_NETWORK')
+c.DockerSpawner.image = os.environ.get('JUPITER_SPAWNIMAGE')
 c.DockerSpawner.http_timeout = 60
